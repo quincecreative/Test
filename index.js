39e3676093f56b3b05436a169f9d2fd17b258740
quincecreative <162695957+quincecreative@users.noreply.github.com>
2024-11-27 10:18:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ setInterval(() => {
 // Register service worker to control making site work offline
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
+  navigator.serviceWorker.register("sw.js").then(() => {
     console.log("Service Worker Registered");
   });
 }
